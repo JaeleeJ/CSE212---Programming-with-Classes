9e8631e93e10a73157c772f7bab460aac8581aab
Jaelee Jeong <107009434+JaeleeJ@users.noreply.github.com>
2025-06-27 11:29:50 -0400

    Delete prove-05-JaeleeJ directory

--- prove-05-JaeleeJ/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using prove_05;
-
-Console.WriteLine("\n======================\nSet Operations\n======================");
-SetOperations.Run();
-
-Console.WriteLine("\n======================\nFind Pairs\n======================");
-FindPairs.Run();
--- prove-05-JaeleeJ/SetExtensionMethods.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace prove_05;
-
-public static class SetExtensionMethods {
-    public static string AsString(this System.Collections.IEnumerable set) {
-        return "<Set>{" + string.Join(", ", (HashSet<int>)set) + "}";
-    }
-}
