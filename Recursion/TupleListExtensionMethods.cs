23a18a8c4c9fd2753276b7343808d28cb8d456e1
Jaelee Jeong <107009434+JaeleeJ@users.noreply.github.com>
2025-06-27 11:36:47 -0400

    Delete Recursion directory

--- Recursion/TupleListExtensionMethods.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-
-namespace prove_08;
-
-public static class TupleListExtensionMethods {
-    public static string AsString(this IEnumerable list) {
-        return "<List>{" + string.Join(", ", list.Cast<ValueTuple<int, int>>()) + "}";
-    }
-}
